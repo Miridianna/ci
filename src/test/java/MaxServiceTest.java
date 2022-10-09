7651dc141472eb7d5c9a99625272da55ce270ee7
Anna Yakovleva <akovlevaanna47@gmail.com>
2022-10-09 12:31:56 +0300

    Initial commit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MaxServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+public class MaxServiceTest {
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
 }
